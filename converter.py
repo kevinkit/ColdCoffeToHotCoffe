581ade84685fb0f12cdd7d294eb5e7683a87cbef
Kevin Alexander Höfle <kevinahoefle@gmail.com>
2017-02-02 15:00:12 +0100

    Optimizing, checking for capitalized
    Included "continue", so that if one type is found the next checks do not have to be done.
    Included a last check the end that searches for uppercase letters, if there are 3 or more then sth will be shown on the command line

--- converter.py
+++ converter.py
@@ -20,6 +20,7 @@ supported Layers:
     POOLING        ... "Pooling"
     INNER_PRODUCT  ... "InnerProduct"
     SOFTMAX        ... "Softmax"
+    DROPOUT        ... "Dropout"
 """
 
 
@@ -30,36 +31,48 @@ filename = "deploy.prototxt";
 proto_file = open(filename,"r");
 x = proto_file.readlines();
 for i in range(0,len(x)):
-    print x[i]
+    buf = str(x[i]);
+    uppers = [l for l in buf if l.isupper()];
+
+    
     if x[i] == "layers {\n": 
         x[i] = "layer {\n";
 
     if x != len(x)-1:
         if x[i] == "layers\n" and x[i+1] == "{\n": 
             x[i] = "layer\n";
- 
+            continue;
+    
     if x[i] == "  type: CONVOLUTION\n" or x[i] == "type: CONVOLUTION\n":
         x[i] = '  type: "Convolution"';
-
+        continue;
     if x[i] == "  type: RELU\n" or x[i] == "type: RELU\n":
         x[i] = '  type: "ReLU"';
-
+        continue;
     if x[i] == "  type: LRN\n" or x[i] == "type: LRN\n":
         x[i] = '  type: "LRN"';
-
+        continue;
     if x[i] == "  type: POOLING\n" or x[i] == "type: POOLING\n":
         x[i] = '  type: "Pooling"';
-
+        continue;
     if x[i] == "  type: INNER_PRODUCT\n" or x[i] == "type: INNER_PRODUCT\n":
         x[i] = '  type: "InnerProduct"';
-
+        continue;
     if x[i] == "  type: SOFTMAX\n" or x[i] == "type: SOFTMAX\n":
         x[i] = '  type: "Softmax"';
-
-
+        continue;
+    if x[i] == "  type: DROPOUT\n" or x[i] == "type: DROPOUT\n":
+        x[i] = '  type: "Dropout"';
+        continue;
+    
+        
+    if(len(uppers) > 2):
+        print "did not change " + buf + " at line " + str(i) + ", if needed change manually";
+        if "pool" in buf:
+            print "Only pool, so do not change";
+        
 with open("new"+filename,"w") as new_file:
     for item in x:
-        print item
         new_file.write("%s" % item);
     
 
