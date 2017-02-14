17ffc2b9a2655950f9450db69a0fcac6bb93de52
Kevin <kevinahoefle@gmail.com>
2017-02-14 18:20:38 +0100

    Clean up

--- converter.py
+++ converter.py
@@ -25,43 +25,50 @@ supported Layers:
 
 
 #Define the .prototxt
-filename = "deploy.prototxt";
+filename = "VGG_CNN_M.prototxt";
 
 
 proto_file = open(filename,"r");
 x = proto_file.readlines();
 for i in range(0,len(x)):
-    buf = str(x[i]);
+#    buf = str(x[i]).strip().rstrip();
+    buf = x[i].strip().rstrip()
     uppers = [l for l in buf if l.isupper()];
+#    x[i] =  x[i].strip().rstrip()
+#    if 'input_dim' in x[i]:
+#     	number = x[i].find(":") +1;
+#	buf = x[i];
+#	x[i] = 'dim:' + str(buf[number:len(buf)])    
 
     
-    if x[i] == "layers {\n": 
+    if buf == "layers {\n" or buf == "layers {":
         x[i] = "layer {\n";
-
     if x != len(x)-1:
-        if x[i] == "layers\n" and x[i+1] == "{\n": 
+        if buf == "layers\n" and x[i+1] == "{\n": 
             x[i] = "layer\n";
             continue;
-    
-    if x[i] == "  type: CONVOLUTION\n" or x[i] == "type: CONVOLUTION\n":
+    #no longer supported
+    if 'blobs_lr' in x[i] or 'weight_decay' in x[i]:
+	x[i] = ''    
+    if buf == "type: CONVOLUTION\n" or buf == "type: CONVOLUTION":
         x[i] = '  type: "Convolution"\n';
         continue;
-    if x[i] == "  type: RELU\n" or x[i] == "type: RELU\n":
+    if  buf == "type: RELU\n" or buf == "type: RELU" : 
         x[i] = '  type: "ReLU"\n';
         continue;
-    if x[i] == "  type: LRN\n" or x[i] == "type: LRN\n":
+    if buf == "type: LRN\n" or buf == "type: LRN":
         x[i] = '  type: "LRN"\n';
         continue;
-    if x[i] == "  type: POOLING\n" or x[i] == "type: POOLING\n":
+    if  buf  == "type: POOLING\n" or buf  == "type: POOLING":
         x[i] = '  type: "Pooling"\n';
         continue;
-    if x[i] == "  type: INNER_PRODUCT\n" or x[i] == "type: INNER_PRODUCT\n":
+    if buf == "type: INNER_PRODUCT\n" or buf == "type: INNER_PRODUCT":
         x[i] = '  type: "InnerProduct"\n';
         continue;
-    if x[i] == "  type: SOFTMAX\n" or x[i] == "type: SOFTMAX\n":
+    if buf  == "type: SOFTMAX\n" or buf == "type: SOFTMAX":
         x[i] = '  type: "Softmax"\n';
         continue;
-    if x[i] == "  type: DROPOUT\n" or x[i] == "type: DROPOUT\n":
+    if buf == "type: DROPOUT\n" or buf == "type: DROPOUT":
         x[i] = '  type: "Dropout"\n';
         continue;
     
@@ -75,8 +82,3 @@ with open("new"+filename,"w") as new_file:
     for item in x:
         new_file.write("%s" % item);
     
-
-
-
-
-
